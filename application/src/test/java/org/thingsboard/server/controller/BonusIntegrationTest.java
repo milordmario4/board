518bed7817de43082a45ecb4b50f0fd3a73fa06c
milordmario4 <69169723+milordmario4@users.noreply.github.com>
2026-02-06 07:50:13 -0500

    add bonus question with license header

--- application/src/test/java/org/thingsboard/server/controller/BonusIntegrationTest.java
+++ application/src/test/java/org/thingsboard/server/controller/BonusIntegrationTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2016-2026 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.controller;
 
 import org.junit.Test;
@@ -12,6 +27,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * Test d'intégration bonus pour vérifier le bon fonctionnement
+ * des endpoints de base de l'application ThingsBoard.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
